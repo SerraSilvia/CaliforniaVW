ee0688f3a19b9e9cc69ed2faea529b60d56aefd9
miguelgarzon <Miguel.Garzon>
2023-12-15 12:09:38 +0100

    Update finalizacion programa

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,19 @@
 package org.example
 
-fun calcularAmortitzacio(){
-
+fun calculoDegradacionVehiculo(km:Long, precio:Double): Double {
+    //funcion la cual calcula la degradacion del vehiculo conforme pasan los kms.
+    val depreciacionPorcentaje = 0.00001
+    val depreciacion:Double = km * depreciacionPorcentaje
+    val valorActual = precio - (precio * depreciacion/100)
+    return valorActual
 }
+
 
 fun main() {
     println("Escriu el preu de la teva California")
-    val valorReal = readln().toInt()
+    val precio= readln().toDouble()
     println("Escriu els km de la teva California")
-    val km = readln().toInt()
+    val km = readln().toLong()
 
-    println("El valor actual de la teva California és:"+ calcularAmortitzacio())
+    println("El valor actual de la teva California és:"+ calculoDegradacionVehiculo(km, precio))
 }
