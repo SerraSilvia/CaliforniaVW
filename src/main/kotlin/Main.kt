1a69c3148b99e87ed417e6feeecf2c52e9988a95
silvia.serra.7e6 <silvia.serra.7e6@itb.cat>
2023-12-15 10:36:13 +0100

    update programa california

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
 package org.example
 
+fun calcularAmortitzacio(){
+
+}
+
 fun main() {
-    println("Hello World!")
+    println("Escriu el preu de la teva California")
+    val valorReal = readln().toInt()
+    println("Escriu els km de la teva California")
+    val km = readln().toInt()
+
+    println("El valor actual de la teva California és:"+ calcularAmortitzacio())
 }
