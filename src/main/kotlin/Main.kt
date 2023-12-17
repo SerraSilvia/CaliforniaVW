91be2a02c44fc2c896b5b63353d1d518fa8b493e
Silvia <silvia.serra.7e6@itb.cat>
2023-12-17 21:16:19 +0100

    Update project Silvia

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,18 +2,41 @@ package org.example
 
 fun calculoDegradacionVehiculo(km:Long, precio:Double): Double {
     //funcion la cual calcula la degradacion del vehiculo conforme pasan los kms.
-    val depreciacionPorcentaje = 0.00001
+    val depreciacionPorcentaje = 0.00001 //és un percentatge
     val depreciacion:Double = km * depreciacionPorcentaje
     val valorActual = precio - (precio * depreciacion/100)
     return valorActual
 }
 
+fun calcularPreuBase(){
+
+}
+
+fun calcularPreuActual(){
+
+}
 
 fun main() {
-    println("Escriu el preu de la teva California")
-    val precio= readln().toDouble()
+    println("BENVINGUT A TASACIÓ DE VEHICLE")
+    println("escull el número de la opció de la teva VW Califòrnia:")
+    println("1- Grand Califòrnia")
+    println("2- Califòrnia Full Equip")
+    println("3- Sortir")
+
+    do {
+        val modelCalifornia = readln().toInt()
+
+        if (modelCalifornia==1){
+
+        }else if (modelCalifornia==2){
+
+        }else println("L'opció no existeix")
+
+    }while (modelCalifornia !=3)
+
+    val precio= 73490
     println("Escriu els km de la teva California")
     val km = readln().toLong()
 
-    println("El valor actual de la teva California és:"+ calculoDegradacionVehiculo(km, precio))
+    println("El valor actual de la teva California és:"+ calculoDegradacionVehiculo(km))
 }
