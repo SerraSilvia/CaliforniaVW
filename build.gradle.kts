763686324a81fe94fabdb3f6000740e915fcfb53
Silvia <silvia.serra.7e6@itb.cat>
2023-12-23 00:53:43 +0100

    Add Dokka and configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.21"
+    id("org.jetbrains.dokka") version "1.9.10"
 }
 
 group = "org.example"
@@ -10,12 +11,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.9.10")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(20)
+tasks.dokkaHtml.configure {
+    outputDirectory.set(buildDir.resolve("dokka"))
 }
